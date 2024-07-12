6d475ab6dd473cc6aaee9eef9601e38768a9e049
Amir Marmul <amiruddinmarmul@gmail.com>
2024-07-12 23:20:52 +0000

    feat: update redis url conf

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,7 @@ const config = {
     grpc: process.env.APP_GRPC || 'waba-starter-default',
     db: process.env.APP_DB || 'mongodb://root:root@mongo:27017/waba-starter-default-DB?authSource=admin',
     mq: process.env.APP_MQ || 'amqp://root:root@rabbitmq',
+    redis: process.env.REDIS_URL || 'redis://redis',
     jwtSecret: process.env.JWT_SECRET || 'secret',
   }
 }
