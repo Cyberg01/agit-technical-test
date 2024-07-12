8c068a5a890c7b9ec556e1b77e20041ee5816dc4
Amir Marmul <amiruddinmarmul@gmail.com>
2024-07-12 23:28:07 +0000

    feat: remove grpc related config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,6 @@ const config = {
   app: {
     name: 'waba-starter-default',
     mode: process.env.MODE_ENV || 'local',
-    grpc: process.env.APP_GRPC || 'waba-starter-default',
     db: process.env.APP_DB || 'mongodb://root:root@mongo:27017/waba-starter-default-DB?authSource=admin',
     mq: process.env.APP_MQ || 'amqp://root:root@rabbitmq',
     redis: process.env.REDIS_URL || 'redis://redis',
