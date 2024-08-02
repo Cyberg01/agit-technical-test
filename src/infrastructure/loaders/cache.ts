16b5b48e4124c191d0c0df8142d3f032bbb4bd42
Amir Marmul <amiruddinmarmul@gmail.com>
2024-08-03 06:01:03 +0800

    bugfix: fix error while building source

--- src/infrastructure/loaders/cache.ts
+++ src/infrastructure/loaders/cache.ts
@@ -1,26 +1,26 @@
 import { Cache, CacheConfig, Loader } from '@amirmarmul/waba-common';
 import config from '@/config';
 
-const config: CacheConfig = {
-    store: 'file',
-    stores: {
-        array: {
-            driver: 'array'
-        },
-        file: {
-            driver: 'file',
-            path: 'storage/cache/data'
-        },
-        redis: {
-            driver: 'redis',
-            redisUrl: config.app.redis,
-        }
+const cacheConfig: CacheConfig = {
+  store: 'file',
+  stores: {
+    array: {
+      driver: 'array'
     },
-    prefix: config.app.name + '_cache_'
+    file: {
+      driver: 'file',
+      path: 'storage/cache/data'
+    },
+    redis: {
+      driver: 'redis',
+      redisUrl: config.app.redis,
+    }
+  },
+  prefix: config.app.name + '_cache_'
 }
 
 export class CacheLoader extends Loader {
-    register(): void {
-        this.container.set(Cache, new Cache(config));
-    }
+  register(): void {
+    this.container.set(Cache, new Cache(cacheConfig));
+  }
 }
